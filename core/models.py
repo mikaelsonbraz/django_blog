f3e1918ea00ffc2400899651f8a3945aec5df12f
mikaelsonbraz <mikaelsonmrn@gmail.com>
2021-10-22 16:02:09 -0300

    corrigindo uma cagadinha

--- core/models.py
+++ core/models.py
@@ -7,4 +7,4 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE) #  ondelete=models.CASCADA gerará um efeito cascata que fará que o post seja deletado caso o autor seja deletado
     body = models.TextField()  # Corpo do post
     created = models.DateTimeField(auto_now_add=True)  # A data do post, auto_now_add=True adiciona automaticamente a data e hora que o post foi criado
-    updated = models.DateTimeField(auto_now=True)  # Esse campo gravará a última data e hora que o post foi MODIFICADO
+    updated = models.DateTimeField(auto_now=True)  # Esse campo gravará a última data e hora que o post foi MODIFICADO.
