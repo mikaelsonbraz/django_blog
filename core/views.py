bddf1a267b1307d49bf31bdadee1fa1b316d4a40
mikaelsonbraz <mikaelsonmrn@gmail.com>
2021-10-29 11:12:19 -0300

    criando urls.py e criando views para listar e detalhar as views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+#  ListView - Listar os posts criados
+#  DetailView - mostrar os detalhes de um post
 
-# Create your views here.
+from .models import Post
+
+
+class PostListView(ListView):
+    model = Post
+
+
+class PostDetailView(DetailView):
+    model = Post
